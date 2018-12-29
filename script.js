b6c8430d6398716b40ef97cb1d3b1f3ae0594150
Joshua Riley <joshrileymt@gmail.com>
2018-12-29 11:45:58 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//mobile nav toggle
+let mainNav = document.getElementById('js-menu');
+
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+
+navBarToggle.addEventListener('click', function () {
+
+    mainNav.classList.toggle('active');
+
+});
+
+
+//slideshow
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 3500); // Change image every 2 seconds
+}
